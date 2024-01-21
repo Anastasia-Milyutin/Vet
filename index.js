b8fd755886acc23a2c94eaaaaaa08a77bc51cd94
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2024-01-21 17:59:30 +0600

    .

--- index.js
+++ index.js
@@ -14,34 +14,43 @@ document
     cityText.style.color = "#FFC59E";
   });
 
+const buttonHeader = document.querySelector(".header__city");
+const modulWindow = document.querySelector(".city__block");
+const buttonClose = document.querySelector(".city__img");
+buttonHeader.addEventListener("click", () => {
+  modulWindow.style.display = "block";
+});
+buttonClose.addEventListener("click", () => {
+  modulWindow.style.display = "none";
+});
 
 
 
-const buttonHeader = document.querySelector('.header__city')
-const modulWindow = document.querySelector('.city__block')
-const buttonClose = document.querySelector('.city__img')
-buttonHeader.addEventListener('click', () => {
-  modulWindow.style.display = 'block'
-})
-buttonClose.addEventListener('click', () => {
-  modulWindow.style.display = 'none'
-})
 
+document.querySelector(".welcome_start").style.display = "block";
 
+const btns = document.querySelectorAll(".welcome__btn");
+const blocks = document.querySelectorAll(".welcome__block");
 
-const buttons = document.querySelectorAll('.welcome__btn');
+btns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    btn.classList.add("welcome__btn_start");
 
-// Получаем все секции с классом welcome
-const sections = document.querySelectorAll('.welcome');
+    btns.forEach((otherbtn) => {
+      if (otherbtn !== btn) {
+        otherbtn.classList.remove("welcome__btn_start");
+      }
+    });
 
-buttons.forEach(button => {
-  // При клике на кнопку добавляем ей класс welcome__btn_start
-  button.addEventListener('click', () => {
-    button.classList.add('welcome__btn_start');
-    
-    // Проходим по всем секциям и добавляем им класс welcome_start
-    sections.forEach(section => {
-      section.classList.add('welcome_start');
+    blocks.forEach((block, blockindex) => {
+      if (blockindex === index) {
+        block.style.display = "flex";
+        block.style.position = "absolute";
+        block.style.right = "0";
+        block.style.top = "600px";
+      } else {
+        block.style.display = "none";
+      }
     });
   });
 });
